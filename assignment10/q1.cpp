0c16d9f68def8e53dc040765cfc35f93ff8f189c
Saumya Singh <saumyas1706@gmail.com>
2025-12-04 10:59:06 +0530

    Create q1.cpp

---
+++ assignment10/q1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> st;
+    for(int x : nums) {
+        if(st.count(x)) return true;
+        st.insert(x);
+    }
+    return false;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1};
+    cout << (containsDuplicate(nums) ? "true" : "false");
+}
