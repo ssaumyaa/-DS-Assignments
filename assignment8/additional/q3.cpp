922740bee3388c2edc97439fb62607625a8e7c5c
Saumya Singh <saumyas1706@gmail.com>
2025-12-04 10:40:46 +0530

    Create q3.cpp

---
+++ assignment8/additional/q3.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
+int maxDepth(TreeNode* root) {
+    if (!root) return 0;
+    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+
+    cout << "Max Depth = " << maxDepth(root) << endl;
+    return 0;
+}
